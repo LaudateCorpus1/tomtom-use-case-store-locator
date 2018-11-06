b9cbf3ed84d3da21910e2f1a8535938035fc0c40
Anna Kuzniak <Anna.Kuzniak@tomtom.com>
2018-11-06 13:12:00 +0100

    Initial commit

---
+++ stores.js
@@ -0,0 +1,144 @@
+var stores = {
+  "type": "FeatureCollection",
+  "features": [{
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.373564,
+          4.892951
+        ]
+      },
+      "properties": {
+        "address": "Valkensteeg 3, 1012 MH",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.369741,
+          4.891881
+        ]
+      },
+      "properties": {
+        "address": "Rokin 92I 1012 KZ",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.369070,
+          4.901278
+        ]
+      },
+      "properties": {
+        "address": "Waterlooplein 29A, 1011 NX",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.371760,
+          4.894093
+        ]
+      },
+      "properties": {
+        "address": "Pieter Jacobszstraat 10D, 1012 HL",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          52.372008,
+          4.900800
+        ]
+      },
+      "properties": {
+        "address": "Nieuwmarkt 107, 1011 MA",
+        "city": "Amsterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.9202978,
+          4.4787379
+        ]
+      },
+      "properties": {
+        "address": "Coolsingel 105, 3012 AG",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.915708,
+          4.476373
+        ]
+      },
+      "properties": {
+        "address": "Witte de Withstraat 63 3012 BN",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.922971,
+          4.468606
+        ]
+      },
+      "properties": {
+        "address": "Weena 691, 3013 AM ",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.925560,
+          4.483948
+        ]
+      },
+      "properties": {
+        "address": "Goudsesingel 411, 3032 EN",
+        "city": "Rotterdam"
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          51.929622,
+          4.472817
+        ]
+      },
+      "properties": {
+        "address": "Paap Dirckstraat 68, 3032 SW",
+        "city": "Rotterdam"
+      }
+    }
+  ]
+}
